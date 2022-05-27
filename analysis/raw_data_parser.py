34bdfc7ffe7a79f9e971ed9772e79eddbddc0649
hsweif <alex93179@gmail.com>
2022-05-27 16:11:11 -0500

    refactor: :coffin: remove those deprecated code

--- analysis/raw_data_parser.py
+++ analysis/raw_data_parser.py
@@ -1,51 +1,6 @@
 from analysis.utils import get_all_txt_files
-from analysis.sheet_manager import SheetManager
 from analysis.data_point import DataPoint
 from typing import Dict, List
-
-
-def extract_rows_from_file(file_path, work_sheet, row):
-    with open(file_path, 'r') as f:
-        reading_flag = False
-        for line in f.readlines():
-            columns = line.split()
-            if line.startswith('Sample Name'):
-                # there should be a line with format like 'Sample Name CM24-A'
-                # which matches the condition here, and we want to get the sample name
-                # after the prefix, as there might be blanks, we concat the columns left
-                # I simplified the legacy conditions in the ipynb by using a concat idiom
-                sample_name = ''.join(columns[2:])
-                work_sheet.cell(row=row, column=1, value=sample_name)
-
-            if line.startswith('Peak#'):
-                reading_flag = True
-
-            if reading_flag:
-                if len(columns) > 1:
-                    row = row+1
-                    for c in range(6):
-                        try:
-                            val = float(columns[c])
-                        except ValueError:
-                            val = columns[c]
-                        work_sheet.cell(row=row, column=c+1, value=val)
-                else:
-                    # the blank line separte the peak table and the following blocks
-                    # we can break here since we only care about the peak table
-                    break
-
-    return row+2
-
-
-def process_raw_data(data_dir: str, sheet_manager: SheetManager):
-    work_sheet = sheet_manager.load_raw_sheet()
-    all_txt_files = get_all_txt_files(data_dir)
-    # openpyxl writes cell starting from 1 instead of 0
-    row = 1
-    for file in all_txt_files:
-        file_path = data_dir / file
-        row = extract_rows_from_file(file_path, work_sheet, row)
-    sheet_manager.save_workbook()
 
 
 def parse_data_point_from_columns(columns: List) -> DataPoint:
--- analysis/sheet_manager.py
+++ analysis/sheet_manager.py
@@ -13,28 +13,17 @@ class SheetManager:
         self.quant_key = 'Quantification w IS,ES'
         self.external_standard_key = 'EXT_STD'
         self.concentration_key = 'Corrected Concentration'
-        # TODO: remove
-        self.peak_id_key = 'Peak_ID'
-        self.quant_full_key = 'Quantification w IS,ES-full'
         self.wb = openpyxl.Workbook()
 
     def create(self):
         self.wb.create_sheet(title=self.preprocess_key)
-        # self.wb.create_sheet(title=self.peak_id_key)
         self.wb.create_sheet(title=self.quant_key)
-        # self.wb.create_sheet(title=self.quant_full_key)
         self.wb.create_sheet(title=self.external_standard_key)
         self.wb.create_sheet(title=self.concentration_key)
         del self.wb['Sheet']
 
     def load_raw_sheet(self):
         return self.wb[self.preprocess_key]
-
-    def load_raw_data_frames(self):
-        df = pd.read_excel(
-            self.file_path, sheet_name=self.preprocess_key, header=None
-        )
-        return df
 
     def load_quant_sheet_data_frame(self, use_cols=None) -> pd.DataFrame:
         df = pd.read_excel(
@@ -45,26 +34,11 @@ class SheetManager:
     def load_concentration_sheet(self):
         return self.wb[self.concentration_key]
 
-    # TODO: deprecate
-    def load_peak_id_sheet(self):
-        return self.wb[self.peak_id_key]
-
     def load_external_standard_sheet(self):
         return self.wb[self.external_standard_key]
 
     def load_quant_sheet(self):
         return self.wb[self.quant_key]
-
-    # TODO: deprecate
-    def load_quant_full_sheet(self):
-        return self.wb[self.quant_full_key]
-
-    # TODO: deprecate
-    def drop_tmp_sheets(self):
-        # it is a tmp workaround to drop the quant full sheet
-        # we shd remove that sheet from the logic itself for long term if have time
-        del self.wb[self.quant_full_key]
-        self.save_workbook()
 
     def write_raw_data_points(self, dp_dict: Dict[str, List[DataPoint]], vials: List[str]):
         sheet = self.load_raw_sheet()
